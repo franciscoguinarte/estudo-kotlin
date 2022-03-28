21aea5c62c8a8765f4135a397d2fc059d4653c2b
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-28 20:31:16 -0300

    Criando uma classe e instanciando um objeto

---
+++ src/main/kotlin/poo/Classe.kt
@@ -0,0 +1,17 @@
+package poo
+
+class Classe {
+    public var atributo: Int = 0
+    public var atributo2: String = "";
+    public var atributo3: Boolean = false;
+    public var atributo4: Double = 0.0;
+
+    fun imprime() {
+        println("atributo: $atributo")
+        println("atributo2: $atributo2")
+        println("atributo3: $atributo3")
+        println("atributo4: $atributo4")
+    };
+
+
+}
---
+++ src/main/kotlin/poo/Main.kt
@@ -0,0 +1,16 @@
+package poo
+
+//Instancia dois objetos da classe Classe e imprime todos seus atributos
+fun main() {
+
+    var objeto1 = Classe()
+
+    objeto1.atributo = 1
+    objeto1.atributo2 = "Atributo 2"
+    objeto1.atributo3 = true
+    objeto1.atributo4 = 3.14
+
+    objeto1.imprime();
+
+
+}
