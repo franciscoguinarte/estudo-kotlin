6be2d7239d22c63a13c9fac20e9e1a8a70915b4e
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-28 15:21:18 -0300

    Estruturas de condição IF e WHEN

---
+++ src/main/kotlin/CondicionalIF.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+   var valorPositivo: Int = 1;
+   var valorNegativo: Int = -1;
+
+    if(valorPositivo > 0) {
+        println("O valor é positivo");
+    }
+    if (valorNegativo < 0) {
+        println("O valor é negativo");
+    }
+    if (valorPositivo == 0) {
+        println("O valor é zero");
+    }
+
+
+
+}
---
+++ src/main/kotlin/CondicionalWhen.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+   var valor: Int = 1;
+   when (valor) {
+      1 -> println("Valor igual a 1")
+      2 -> println("Valor igual a 2")
+      else -> println("Valor diferente de 1 e 2")
+   }
+}
