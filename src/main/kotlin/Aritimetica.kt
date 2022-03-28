5f220b1f94bf2a4c9e3dd74b309ee9b5c643a7e9
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-28 15:12:36 -0300

    Estudo de aritimetica e tipagem de variaveis em Kotlin

---
+++ src/main/kotlin/Aritimetica.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var soma = 1 + 2
+    var subtracao = 1 - 2
+    var multiplicacao = 1.0 * 2
+    var divisao = (1.0 / 2.0)
+    var modulo = 1 % 2
+
+    println("Soma: $soma")
+    println("Subtracao: $subtracao")
+    println("Multiplicacao: $multiplicacao")
+    println("Divisao: $divisao")
+    println("Modulo: $modulo")
+    println("Incremento: ${++soma}")
+    println("Decremento: ${--soma}")
+
+
+}
---
+++ src/main/kotlin/Tipagem.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    var variavelString : String = "Essa é uma string"
+    var variavelInt : Int = 1 // Esse é um inteiro
+    var variavelDouble : Double = 1.0 // Esse é um double
+    var variavelBoolean : Boolean = true // Esse é um boolean
+    var variavelChar : Char = 'a' // Esse é um char
+    var variavelFloat : Float = 1.0f // Esse é um float
+    var variavelLong : Long = 1L // Esse é um long
+    var variavelShort : Short = 1 // Esse é um short
+    var variavelByte : Byte = 1 // Esse é um byte
+    var variavelAny : Any = 1 // Esse é um any
+
+
+    println("Escrevendo a string $variavelString")
+    println("Escrevendo o inteiro $variavelInt")
+    println("Escrevendo o double $variavelDouble")
+    println("Escrevendo o boolean $variavelBoolean")
+    println("Escrevendo o char $variavelChar")
+    println("Escrevendo o float $variavelFloat")
+    println("Escrevendo o long $variavelLong")
+    println("Escrevendo o short $variavelShort")
+    println("Escrevendo o byte $variavelByte")
+    println("Escrevendo o any $variavelAny")
+
+
+}
