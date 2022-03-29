6d400c71b0752f79c9e709bd3ee3ee825030ba40
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-28 23:26:26 -0300

    Adicionando get e set em Classe

--- src/main/kotlin/poo/Classe.kt
+++ src/main/kotlin/poo/Classe.kt
@@ -1,17 +1,25 @@
 package poo
 
-class Classe {
-    public var atributo: Int = 0
-    public var atributo2: String = "";
-    public var atributo3: Boolean = false;
-    public var atributo4: Double = 0.0;
+public class Classe {
+    private var atributo: Int = 0
+    private var atributo2: String = "";
+    private var atributo3: Boolean = false;
+    private var atributo4: Double = 0.0;
 
-    fun imprime() {
-        println("atributo: $atributo")
-        println("atributo2: $atributo2")
-        println("atributo3: $atributo3")
-        println("atributo4: $atributo4")
-    };
+    constructor(atributo: Int, atributo2: String, atributo3: Boolean, atributo4: Double) {
+        this.atributo = atributo
+        this.atributo2 = atributo2
+        this.atributo3 = atributo3
+        this.atributo4 = atributo4
+    }
+
+    fun getAtributo(): Int {
+        return atributo
+    }
+    fun setAtributo(atributo: Int) {
+        this.atributo = atributo
+    }
+
 
 
 }
