c5abd259431a2fe496bd27bfabca09675f8e6cd9
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-29 23:53:27 -0300

    Finalizando primeiro modulo formacao Kotlin

--- src/main/kotlin/poo/Classe.kt
+++ src/main/kotlin/poo/Classe.kt
@@ -1,25 +1,15 @@
 package poo
 
 public class Classe(
-    val atributo: Int,
-    val atributo2: String,
-    var atributo3: String,
-    var atributo4: String
+    atributo: Int,
+    atributo2: String,
+    atributo3: Boolean,
+    atributo4: Double
 ) {
 
+    var atributo = atributo
+    val atributo2 = atributo2
+    var atributo3 = atributo3
+    var atributo4 = atributo4
 
-//    constructor(atributo: Int, atributo2: String, atributo3: Boolean, atributo4: Double) : this() {
-//        this.atributo = atributo
-//        this.atributo2 = atributo2
-//        this.atributo3 = atributo3
-//        this.atributo4 = atributo4
-//    }
-
-
-//    fun getAtributo(): Int {
-//        return atributo
-//    }
-//    fun setAtributo(atributo: Int) {
-//        this.atributo = atributo
-//    }
 }
--- src/main/kotlin/poo/Main.kt
+++ src/main/kotlin/poo/Main.kt
@@ -3,13 +3,12 @@ package poo
 //Instancia dois objetos da classe Classe e imprime todos seus atributos
 fun main() {
 
-    var objeto1 = Classe(
-        1,
-        "Nome 1",
-        true,
-        1.0,
-    )
+    var obj1 = Classe(1,"String", true, 2.00)
 
-    objeto1.atributo = 2
+    obj1.atributo3 = false
+    println(obj1.atributo2)
+    println(obj1.atributo)
+
+
 
 }
